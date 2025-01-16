4ffac119da6159ceedd2218ad73dccc8afdb46ce
hamalwa <hamalwa@neubaukompass.de>
2025-01-16 11:24:05 +0100

    Changes to next.config.ts disabling experimental features

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const isProd = process.env.NODE_ENV === "production";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export", // Enable static export
+    assetPrefix: isProd ? "/" : "", // Use "/" in production for `next/font`
+    images: {
+        unoptimized: true, // Disable image optimisation for compatibility with static exports
+    }
 };
 
 export default nextConfig;
