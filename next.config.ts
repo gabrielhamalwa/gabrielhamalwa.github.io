df44adaec5e0e4f2b6779492b7071a1df1b87f0c
hamalwa <hamalwa@neubaukompass.de>
2025-01-16 11:40:36 +0100

    Deploy updated static files

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,12 @@ const isProd = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export", // Enable static export
-    assetPrefix: isProd ? "/" : "", // Use "/" in production for `next/font`
+    output: "export", // Enables static export
+    assetPrefix: isProd ? "https://gabrielhamalwa.github.io/" : "", // Full URL for GitHub Pages
+    basePath: isProd ? "/gabrielhamalwa.github.io" : "", // Base path for GitHub Pages
     images: {
-        unoptimized: true, // Disable image optimisation for compatibility with static exports
-    }
+        unoptimized: true, // Disable image optimization for static export
+    },
 };
 
 export default nextConfig;
